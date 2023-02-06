7f2cc4c6610a5390052866c592705be86afce9e3
NonQuant <husainov.21.04@gmail.com>
2023-02-07 04:23:23 +0500

    main function returns boolean type instead of int. There is no need for probability

--- evaluate.py
+++ evaluate.py
@@ -1,78 +1,64 @@
-import cv2
-import imutils
+import os
+import torch
+import torch.nn as nn
 import numpy as np
-from keras.models import load_model
-from matplotlib import pyplot as plt
+from PIL import Image
+from torchvision import transforms, models
 
-img_width, img_height = 240, 240
+device_name = "cuda:0:" if torch.cuda.is_available() else "cpu"
+device = torch.device(device_name)
+transform = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor()])
 
-MODEL_PATH = "cnn-parameters-improvement-23-0.91.model"
-best_model = load_model(filepath=MODEL_PATH)
+resnet_model = models.resnet50(pretrained=True)
 
+for param in resnet_model.parameters():
+    param.requires_grad = True
 
-def _convert_grayscale(img):
-    # Convert the img to grayscale, and blur it slightly
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    return gray
+n_inputs = resnet_model.fc.in_features
 
+resnet_model.fc = nn.Sequential(nn.Linear(n_inputs, 2048),
+                                nn.SELU(),
+                                nn.Dropout(p=0.4),
+                                nn.Linear(2048, 2048),
+                                nn.SELU(),
+                                nn.Dropout(p=0.4),
+                                nn.Linear(2048, 4),
+                                nn.LogSigmoid())
 
-def _clear_img(img):
-    # Threshold the img, then perform a series of erosions +
-    # dilations to remove any small regions of noise
-    thresh = cv2.threshold(img, 45, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.erode(thresh, None, iterations=2)
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    return thresh
+for name, child in resnet_model.named_children():
+    for name2, params in child.named_parameters():
+        params.requires_grad = True
 
+resnet_model.to(device)
 
-def _grab_largest_contour(img):
-    cntrs = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntrs = imutils.grab_contours(cntrs)
-    cntr_max = max(cntrs, key=cv2.contourArea)
-    return cntr_max
+resnet_model.load_state_dict(torch.load('models\\bt_resnet50_model.pt', map_location=torch.device('cpu')))
 
+resnet_model.eval()
 
-def crop_brain_contour(img, plot=False):
-    # segment the brain from the rest of the img
-    gray = _convert_grayscale(img)
-    thresh = _clear_img(gray)
-    cntr = _grab_largest_contour(thresh)
+LABELS = ['Опухоль не обнаружена.', 'Обнаружена менингиома!', 'Обнаружена глиома!', 'Обнаружена аденома гипофиза!']
 
-    # Find the extreme points
-    extLeft = tuple(cntr[cntr[:, :, 0].argmin()][0])
-    extRight = tuple(cntr[cntr[:, :, 0].argmax()][0])
-    extTop = tuple(cntr[cntr[:, :, 1].argmin()][0])
-    extBot = tuple(cntr[cntr[:, :, 1].argmax()][0])
-
-    # crop new img out of the original img using the four extreme points (left, right, top, bottom)
-    new_img = img[extTop[1]:extBot[1], extLeft[0]:extRight[0]]
-
-    if plot:
-        plt.figure()
-        plt.subplot(1, 2, 1)
-        plt.imshow(img)
-        plt.tick_params(axis='both', which='both',
-                        top=False, bottom=False, left=False, right=False,
-                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
-        plt.title('Original img')
-
-        plt.subplot(1, 2, 2)
-        plt.imshow(new_img)
-        plt.tick_params(axis='both', which='both',
-                        top=False, bottom=False, left=False, right=False,
-                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
-        plt.title('Cropped img')
-        plt.show()
-
-    return new_img
+print("done")
 
 
-def predict(filepath):
-    img = cv2.imread(filepath)
-    img = crop_brain_contour(img, plot=False)
-    img = cv2.resize(img, dsize=(img_width, img_height), interpolation=cv2.INTER_CUBIC)
-    img = img / 255.  # normalize values
+def classify(filename) -> int:
+    if not os.path.exists(filename):
+        print("File does not exits. Exiting...\n")
+        exit()
 
-    imgs = np.array([img])
-    return round(best_model.predict(imgs)[0][0], 2) * 100
+    img = Image.open(filename)
+
+    img = transform(img)
+
+    img = img[None, ...]
+
+    with torch.no_grad():
+        y_hat = resnet_model.forward(img.to(device))
+
+        predicted = torch.argmax(y_hat.data, dim=1)
+
+        return int(predicted.data[0])
+
+# Code for testing
+#
+# for file in os.listdir("augmented data/no/")[:10]:
+#     print(classify(f"augmented data/no/{file}"))
